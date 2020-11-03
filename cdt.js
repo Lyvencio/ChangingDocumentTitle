d2efb520089ce22cac5ebf1ee7a328ef05f72c26
Lyvencio <73907599+Lyvencio@users.noreply.github.com>
2020-11-04 00:01:32 +0100

    Update cdt.js

--- cdt.js
+++ cdt.js
@@ -1,5 +1,4 @@
-// Script (ChangingDocumentTitle) by Lyvencio                // https://github.com/Lyvencio/ 
-// Fade the original page title in letter by letter         // https://github.com/Lyvencio/ChangingDocumentTitle
+// Letter-by-letter fade-in <title> script                  // https://github.com/Lyvencio
 var interval = 300                                         // Recommended interval of letter-by-letter fade-in
 var title = this.document.title;                          // Defining title
 this.document.title = '⠀'                                // Removing title (invisible character)
@@ -10,4 +9,4 @@ var updatingtitle = title.substring(0, character);   // Substring from title
 character += 1;                                     // Adding next letter
 this.document.title = updatingtitle;               // Setting new title
 }                                                 // Ending Changetitle Function
-// Hope you like my scripts! Send me an email at // lyvencio@gmail.com
+// Script made by Lyvencio                       // https://github.com/Lyvencio
